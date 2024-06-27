d5ea09786b307d87c6292b0005d4b4e9c27ba422
Michael Dyer <Dyerm636@gmail.com>
2024-06-27 16:38:56 -0500

    fixing mistake with line 54

--- candidate-testing.js
+++ candidate-testing.js
@@ -51,7 +51,7 @@ function gradeQuiz(candidateAnswers) {
   for (let i = 0; i < questions.length; i++){
 
     if (candidateAnswers[i] == ""){
-      console.log(`\nYou did not answer questioncorrectAnswers[i].toLowerCase() == candidateAnswers[i].toLowerCase() ${i+1}.\n`);
+      console.log(`\nYou did not answer question ${i+1}.\n`);
     }
     else {
       console.log(`\nQuestion ${i+1}'s answer was ${correctAnswers[i]}: You answered ${candidateAnswers[i]}`);
